b582c0c8c8627568a21290fb4e9041df44fd8692
Marc Andersen <marchot1999@gmail.com>
2021-08-09 13:19:03 +0200

    Add project files.

---
+++ HttpRequest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace HttpRequest
+{
+    class Program 
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main()
+        {
+            try
+            {
+                HttpResponseMessage respons = await client.GetAsync(@"https://ugenr.dk/");
+                respons.EnsureSuccessStatusCode();
+                string responsBody = await respons.Content.ReadAsStringAsync();
+
+                Console.WriteLine(Regex.Replace(responsBody, "<[^>]*>", "Removed"));
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
